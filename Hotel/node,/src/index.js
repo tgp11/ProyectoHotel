090136b5312bd3f4b4cb095777bac3e4ec835108
Ivan <ivanrex131@gmail.com>
2026-01-17 19:03:02 +0100

    Añadidas las rutas de cliente y empleado al index

--- Hotel/node,/src/index.js
+++ Hotel/node,/src/index.js
@@ -7,10 +7,14 @@ app.use(express.json());
 const usuarioRoutes = require('./usuario/usuario.routes');
 const habitacionRoutes = require('./habitacion/habitacion.routes');
 const reservaRoutes = require('./reserva/reserva.routes');
+const clienteRoutes = require('./cliente/cliente.routes');
+const empleadoRoutes = require('./Empleado/empleado.routes');
 
 app.use('/usuarios', usuarioRoutes);
 app.use('/habitaciones', habitacionRoutes);
 app.use('/reservas', reservaRoutes);
+app.use('/clientes', clienteRoutes);
+app.use('/empleados', empleadoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
