68584f2807d263588dd8115308fa0457f3e0907f
Antonio <antgarpoz@alu.edu.gva.es>
2026-02-02 20:30:37 +0100

    WPF

---
+++ Hotel/csharp/HOTELINTERFAZ/HOTELINTERFAZ/Models/Reserva.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace HOTELINTERFAZ.Models
+{
+    public class Reserva
+    {
+        [JsonPropertyName("_id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("clienteId")]
+        public string ClienteId { get; set; }
+
+        [JsonPropertyName("habitacionId")]
+        public string HabitacionId { get; set; }
+
+        [JsonPropertyName("fechaEntrada")]
+        public DateTime FechaEntrada { get; set; }
+
+        [JsonPropertyName("fechaSalida")]
+        public DateTime FechaSalida { get; set; }
+
+        [JsonPropertyName("personas")]
+        public int Personas { get; set; }
+
+        [JsonPropertyName("precioTotal")]
+        public double PrecioTotal { get; set; }
+
+        [JsonPropertyName("cancelacion")]
+        public bool Cancelacion { get; set; }
+    }
+}
