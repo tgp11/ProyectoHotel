61aceceaf2566908a0981f7e5c851ce74f218cfc
Ivan <ivanrex131@gmail.com>
2026-02-06 19:00:13 +0100

    Quick fix

--- Hotel/node,/src/db.js
+++ Hotel/node,/src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const URI = 'mongodb://localhost:27017/Hotel
+const URI = 'mongodb://localhost:27017/Hotel'; 
 //const URI = 'mongodb://localhost:27017/HotelPereMaria'; // tu base de datos
 mongoose.connect(URI) // sin opciones obsoletas
   .then(() => console.log('Conectado a la base de datos'))
