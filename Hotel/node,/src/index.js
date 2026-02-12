57d36b4d61873b6bbd21d4d0eded4257afa3caee
Ivan <ivanrex131@gmail.com>
2026-02-12 19:23:16 +0100

    Capacidad de guardar fotos de usuarios añadida

--- Hotel/node,/src/index.js
+++ Hotel/node,/src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const cors = require('cors'); 
 const app = express();
+const path = require('path');
+const multer = require("multer");
 const PORT = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
 
 // Conexión a DB (usa tu archivo db.js)
 require('./db'); 
@@ -9,6 +13,16 @@ require('./db');
 // Middlewares - ¡IMPORTANTE!
 app.use(cors()); 
 app.use(express.json()); 
+app.use("/uploads", express.static("uploads"));
+
+//Error que salta en caso de que el formato de imagen no sea permitido o haya un error con multer
+app.use((err, req, res, next) => {
+    if (err instanceof multer.MulterError || err.message.includes("Formato")) {
+        return res.status(400).json({ msg: err.message });
+    }
+
+    next(err);
+});
 
 // Rutas
 const reservaRoutes = require('./reserva/reserva.routes');
